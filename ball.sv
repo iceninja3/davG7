aeca001ebd37c940c1f6fdb94214ccdd2976a4f1
hiroglyph22 <hirotrepreneur@gmail.com>
2025-04-13 17:16:21 -0700

    Added reset to ball

--- ball.sv
+++ ball.sv
@@ -2,6 +2,7 @@ module Ball #(parameter x_coords_width = 10,
               parameter y_coords_width = 10)
 (
   input clk,
+  input reset,
   input touching_paddle,
   input touching_wall,
   input [x_coords_width-1: 0] old_x,
@@ -13,7 +14,14 @@ module Ball #(parameter x_coords_width = 10,
   reg x_speed_sign = 0;
   reg y_speed_sign = 0;
 
+
+
   always @(posedge clk) begin
+
+    if(reset) begin
+        x_speed_sign <= 0;
+        y_speed_sign <= 0;
+    end
 
     if (touching_paddle)
       x_speed_sign <= ~x_speed_sign;
