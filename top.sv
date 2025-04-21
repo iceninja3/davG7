b875f6b13ec447d44a224c7e98fc1a50026fd44b
Aditya Datar <127159762+adityadatar8@users.noreply.github.com>
2025-04-20 17:10:17 -0700

    Update top.sv

--- top.sv
+++ top.sv
@@ -1,9 +1,16 @@
-module top(
-    input clk,
-    input rst,
-    input player1,
-    input player2
-);
+`define BIT_WIDTH 8
+`define BALL_RADIUS 4
+`define PADDLE_RADIUS 8
+`define MID_X 40
+`define FLOOR_Y 80
+
+module top
+    (
+        input clk,
+        input rst,
+        input player1,
+        input player2
+    );
 
     reg ball_x = 0;
     reg ball_y = 0;
@@ -16,7 +23,7 @@ module top(
     paddle paddle1(clk, rst, player1, player1_x);
     paddle paddle2(clk, rst, player2, player2_x);
     ball ball1(clk, rst, paddle_collision, wall_collision, ball_x, ball_y);
-    collisionDetection collisionDetector()
+    collisionDetection collisionDetector (BIT_WIDTH, BALL_RADIUS, PADDLE_RADIUS, MID_X, FLOOR_Y);
 
     always @(posedge clk) begin
         
