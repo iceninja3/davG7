43513b7deeb4f06ce4a6c8b529461d220dd9af60
Aditya Datar <127159762+adityadatar8@users.noreply.github.com>
2025-05-09 22:02:34 -0700

    Update collisionDetection.sv

--- collisionDetection.sv
+++ collisionDetection.sv
@@ -1,14 +1,15 @@
-module collisionDetection #(parameter BIT_WIDTH, BALL_RADIUS, Y_AXIS)
+module collisionDetection #(parameter BIT_WIDTH, BALL_RADIUS, PADDLE_RADIUS MID_X, FLOOR_Y)
     (
-        input logic[BIT_WIDTH:0] paddleX,
-        input logic[BIT_WIDTH:0] paddleY,
-        input logic[BIT_WIDTH:0] ballX,
-        input logic[BIT_WIDTH:0] ballY,
+        input logic[(BIT_WIDTH-1):0] paddleX,
+        input logic[(BIT_WIDTH-1):0] paddleY,
+        input logic[(BIT_WIDTH-1):0] ballX,
+        input logic[(BIT_WIDTH-1):0] ballY,
 
         output logic touchingPaddle,
         output logic touchingFloor
     );
 
-    assign touchingPaddle = ((ballX == paddleX) && (ballY == paddleY));
+    assign touchingPaddle = ((ballX == paddleX) && (ballY <= (paddleY + PADDLE_RADIUS)) && (ballY >= (paddleY - PADDLE_RADIUS)));
+    assign touchingFloor = ((ballY - BALL_RADIUS) == FLOOR_Y);
 
 endmodule
